20fb06b8566bcbc04c6c2e273c2a2a7722b26201
Achira-Oulu <achira.hendalage@student.oulu.fi>
2024-02-19 08:56:04 +0200

    padding bottom

--- src/FixedBottomNavigation.js
+++ src/FixedBottomNavigation.js
@@ -81,9 +81,18 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import Paper from '@mui/material/Paper';
 
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 export default function FixedBottomNavigation() {
     const location = useLocation();
     const [value, setValue] = useState(0);
+
+
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.up('sm'));
+
+
 
     const getNavigationValueFromPath = (path) => {
         switch (path) {
@@ -109,7 +118,13 @@ export default function FixedBottomNavigation() {
     return (
         <Box sx={{ pb: 7 }}>
             <CssBaseline />
-            <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+            <Paper sx={{ 
+                position: 'fixed', 
+                bottom: matches ? 'env(safe-area-inset-bottom)' : 0, 
+                left: 0, 
+                right: 0,
+                paddingBottom: 'env(safe-area-inset-bottom)'
+                 }} elevation={3}>
                 <BottomNavigation
                     showLabels
                     value={value}
