3ff07a1dfdd308ff7d397a189ff0189d3ed0b409
Achira-Oulu <achira.hendalage@student.oulu.fi>
2024-02-27 09:52:56 +0200

    fixed fetch address

--- src/Review.js
+++ src/Review.js
@@ -14,7 +14,7 @@ const Review = () => {
         formData.append("audio", audioBlob, "recording.wav");
     
         try {
-            const response = await fetch('http://localhost:5000/upload_audio', {
+            const response = await fetch('https://univibe-back-production.up.railway.app/upload_audio', {
                 method: 'POST',
                 body: formData,
             });
