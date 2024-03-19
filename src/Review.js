b555275250e5e0ebd1c3c60b4f04b7ff3b520310
Achira-Oulu <achira.hendalage@student.oulu.fi>
2024-03-19 15:58:32 +0200

    removed rate icon

--- src/Review.js
+++ src/Review.js
@@ -3,9 +3,9 @@ import { Container, Typography, TextField, Button, Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Avatar from '@mui/material/Avatar';
+// import Avatar from '@mui/material/Avatar';
 // import GroupsIcon from '@mui/icons-material/Groups';
-import RateReviewIcon from '@mui/icons-material/RateReview';
+// import RateReviewIcon from '@mui/icons-material/RateReview';
 import Link from '@mui/material/Link';
 // import Grid from '@mui/material/Grid';
 
@@ -99,9 +99,9 @@ const Review = () => {
                 Please enter the unique ID you've received at the end of the demographic questionnaire to get started. If you haven't filled it, please find it <Link href="https://forms.gle/uDPMdZCamQBgo9CA7" underline="hover">here</Link>.
             </Typography> */}
 
-            <Avatar sx={{ m: 1, bgcolor: 'black' }}>
+            {/* <Avatar sx={{ m: 1, bgcolor: 'black' }}>
                 <RateReviewIcon />
-            </Avatar>
+            </Avatar> */}
             <Typography component="h1" variant="h5">
                 Start your Review
             </Typography>
