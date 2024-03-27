05ddfe4abf83b0372308f5883a5bdf6f5890322c
Achira-Oulu <achira.hendalage@student.oulu.fi>
2024-03-27 14:53:21 +0200

    added loading screen

--- src/common/LoadingScreen.js
+++ src/common/LoadingScreen.js
@@ -1,36 +1,3 @@
-// import * as React from 'react';
-// import CircularProgress from '@mui/material/CircularProgress';
-// import Box from '@mui/material/Box';
-
-// export default function CircularIndeterminate() {
-//   return (
-//     <Box sx={{ display: 'flex' }}>
-//       <CircularProgress />
-//     </Box>
-//   );
-// }
-
-
-
-// import * as React from 'react';
-// import CircularProgress from '@mui/material/CircularProgress';
-// import Box from '@mui/material/Box';
-
-// export default function CircularIndeterminate() {
-//   return (
-//     <Box
-//       sx={{
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         height: '100vh',
-//       }}
-//     >
-//       <CircularProgress />
-//     </Box>
-//   );
-// }
-
 import React, { useState, useEffect } from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
